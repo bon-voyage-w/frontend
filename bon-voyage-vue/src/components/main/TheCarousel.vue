87fd8d0e59ebb6b69e2f91e5bfa54ef46b56c183
prkim99@gmail.com <prkim99@gmail.com>
2023-05-25 23:38:09 +0900

    [feat] 메인화면 그림 고해상도로 수정

--- bon-voyage-vue/src/components/main/TheCarousel.vue
+++ bon-voyage-vue/src/components/main/TheCarousel.vue
@@ -7,14 +7,15 @@
       img-width="1024"
       img-height="480"
     >
+
       <b-carousel-slide
-        img-src="https://picsum.photos/1024/480/?image=54"
+        img-src="https://plus.unsplash.com/premium_photo-1682091715689-b6ab529fd9a3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1771&q=80"
       ></b-carousel-slide>
       <b-carousel-slide
-        img-src="https://picsum.photos/1024/480/?image=12"
+          img-src="https://images.unsplash.com/photo-1609766418204-94aae0ecfdfc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2232&q=80"
       ></b-carousel-slide>
       <b-carousel-slide
-        img-src="https://picsum.photos/1024/480/?image=22"
+          img-src="https://images.unsplash.com/photo-1618590393910-0ffae52e90ac?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2232&q=80"
       ></b-carousel-slide>
     </b-carousel>
   </div>
@@ -28,3 +29,7 @@ export default {
   },
 };
 </script>
+<style>
+b-carousel-slide {
+}
+</style>
