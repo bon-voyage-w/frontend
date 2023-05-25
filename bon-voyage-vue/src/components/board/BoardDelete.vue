5a5583285267384078056804e50d4a8856608dc0
오채영 <web3765@gmail.com>
2023-05-25 18:09:00 +0900

    commit

---
+++ bon-voyage-vue/src/components/board/BoardDelete.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="board-write">
+    <div class="placeinfo-list-title">
+      <h2>공지사항</h2>
+      <p>글수정</p>
+    </div>
+    <b-form-input id="input-default" placeholder="제목을 입력하세요."></b-form-input>
+
+    <b-form-textarea
+      class="board-textarea"
+      id="textarea"
+      v-model="text"
+      placeholder="글을 입력하세요."
+      rows="20"
+      max-rows="6"
+    ></b-form-textarea>
+
+    <button class="board-modify-btn" variant="outline-primary" @click="deleteBoard">삭제</button>
+    <button class="board-list-btn" variant="outline-primary" @click="moveList">목록</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BoardDelete",
+  data() {
+    return {
+      text: "",
+    };
+  },
+};
+</script>
+
+<style>
+.board-write {
+  max-width: 80%;
+  margin: 20px auto;
+}
+.board-write h2 {
+  text-align: center;
+}
+.board-write .board-textarea {
+  margin: 10px 0;
+}
+.board-write .board-write-btn {
+  float: right;
+}
+</style>
