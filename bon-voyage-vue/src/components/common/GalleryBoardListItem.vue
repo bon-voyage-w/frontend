fbeb59b9646d49ff8fea60c77b806d66d79f3cb9
오채영 <web3765.gmail.com>
2023-05-25 19:00:59 +0900

    [style] 관광지 게시글 a tag 밑줄 제거

--- bon-voyage-vue/src/components/common/GalleryBoardListItem.vue
+++ bon-voyage-vue/src/components/common/GalleryBoardListItem.vue
@@ -2,7 +2,10 @@
   <button class="placeinfo-list-item">
     <router-link
       @click="selectAttraction"
-      :to="{ name: 'attractiondetail', params: { contentId: attraction.contentId } }"
+      :to="{
+        name: 'attractiondetail',
+        params: { contentId: attraction.contentId },
+      }"
     >
       <img :src="attraction.firstImage" alt="" @error="replaceNoImg" />
       <div class="placeinfo-list-item-text">
@@ -29,7 +32,8 @@ export default {
       this.detailAttraction(this.attraction.contentId);
     },
     replaceNoImg(e) {
-      e.target.src = "https://cdn.pixabay.com/photo/2014/04/13/20/49/cat-323262_640.jpg";
+      e.target.src =
+        "https://cdn.pixabay.com/photo/2014/04/13/20/49/cat-323262_640.jpg";
     },
   },
 };
@@ -61,5 +65,8 @@ export default {
 .placeinfo-list-item-text p {
   color: #656b72;
   font-size: 0.8em;
+}
+a {
+  text-decoration: none;
 }
 </style>
