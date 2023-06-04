ace759d802f9f0d05927eba733a01cd083b0aeed
오채영 <web3765.gmail.com>
2023-06-04 16:47:24 +0900

    [feat] attraction map 마커 이미지 적용

--- bon-voyage-vue/src/components/attractionBoard/AttractionBoardMap.vue
+++ bon-voyage-vue/src/components/attractionBoard/AttractionBoardMap.vue
@@ -17,6 +17,7 @@ export default {
       map: null,
       positions: [],
       markers: [],
+      markerImages: [],
     };
   },
   computed: {
@@ -24,7 +25,7 @@ export default {
   },
   watch: {
     attractions() {
-      console.log(">>>>>>>>>>>>> 관광지 ::: ", this.attractions.content);
+      // console.log(">>>>>>>>>>>>> 관광지 ::: ", this.attractions.content);
       this.positions = [];
       this.attractions.content.forEach((attraction) => {
         let obj = {};
@@ -33,9 +34,9 @@ export default {
           attraction.latitude,
           attraction.longitude
         );
+        obj.contentTypeId = attraction.contentTypeId;
 
         this.positions.push(obj);
-        console.log("@@@@@@@@@@@@@ maker에 넣는 obj " + obj);
       });
       this.loadMaker();
     },
@@ -77,27 +78,33 @@ export default {
       };
 
       this.map = new window.kakao.maps.Map(container, options);
-
-      // 마커를 생성합니다
-      // var marker = new kakao.maps.Marker({
-      //   position: options.center,
-      // });
-      // marker.setMap(this.map);
     },
     // 지정한 위치에 마커 불러오기
     loadMaker() {
       this.deleteMarker();
 
       this.markers = [];
+      this.markerImages = [];
+      var imageSize = new kakao.maps.Size(40, 40); // 마커이미지의 크기입니다
+
+      this.positions.forEach((position) => {
+        var markerImage = new kakao.maps.MarkerImage(
+          require(`@/assets/marker/${position.contentTypeId}.png`),
+          imageSize
+        );
+        this.markerImages.push(markerImage);
+      });
+
+      var idx = 0;
       this.positions.forEach((position) => {
         const marker = new kakao.maps.Marker({
           map: this.map, // 마커를 표시할 지도
           position: position.latlng, // 마커를 표시할 위치
           title: position.title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+          image: this.markerImages[idx++],
         });
         this.markers.push(marker);
       });
-      console.log("마커수 ::: " + this.markers.length);
 
       // 4. 지도를 이동시켜주기
       // 배열.reduce( (누적값, 현재값, 인덱스, 요소)=>{ return 결과값}, 초기값);
@@ -109,10 +116,9 @@ export default {
       this.map.setBounds(bounds);
     },
     deleteMarker() {
-      // console.log("마커 싹 지우자!!!", this.markers.length);
       if (this.markers.length > 0) {
         this.markers.forEach((item) => {
-          console.log(item);
+          // console.log(item);
           item.setMap(null);
         });
       }
